123fd54a46dc44383fd29dbfd28ffeed8c5f8d25
yurii433 <yuradiachuk1@gmail.com>
2024-07-21 17:58:48 +0200

    another attempt to fix a bug

--- src/server/dist/server.js
+++ src/server/dist/server.js
@@ -1,16 +1,12 @@
 "use strict";
-var __importDefault =
-  (this && this.__importDefault) ||
-  function (mod) {
-    return mod && mod.__esModule ? mod : { default: mod };
-  };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
 const cors_1 = __importDefault(require("cors"));
-const apartmentsRoutes_1 = __importDefault(
-  require("./routes/apartmentsRoutes")
-);
+const apartmentsRoutes_1 = __importDefault(require("./routes/apartmentsRoutes"));
 const usersRoutes_1 = __importDefault(require("./routes/usersRoutes"));
 const mongoConfig_1 = __importDefault(require("./mongoConfig"));
 dotenv_1.default.config();
@@ -22,10 +18,10 @@ app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: false }));
 app.use("/apartments", apartmentsRoutes_1.default);
 app.use("/users", usersRoutes_1.default);
-app.use((req, res) => {
-  res.status(404).json({ error: "Not Found" });
-  console.log("Not found");
+app.use((res) => {
+    res.status(404).json({ error: "Not Found" });
+    console.log("Not found");
 });
 app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
+    console.log(`[server]: Server is running at http://localhost:${port}`);
 });
