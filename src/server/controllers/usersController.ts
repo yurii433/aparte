cef5f0607791a0dc9139bdb085b6628d6b81d942
yurii433 <yuradiachuk1@gmail.com>
2024-08-05 12:34:34 +0200

    testing

--- src/server/controllers/usersController.ts
+++ src/server/controllers/usersController.ts
@@ -23,9 +23,9 @@ const user_signup = async (req: Request, res: Response) => {
     const user = await User.create({ email, password });
 
     const token = createToken(user._id);
-
+    /* 
     res.cookie("jwt", token, { httpOnly: true, maxAge: maxAge * 1000 });
-
+ */
     res.status(201).json({ user: user._id });
   } catch (err) {
     console.log(err);
